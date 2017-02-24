2ffe195a07adfeab562658e4e24b2ee4e5dfa1c3
Zachary Murphy <ztmurphy21@gmail.com>
2017-02-23 21:20:10 -0500

    first assignment

---
+++ Voter Solution/Voter Project/Main Form.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.lblMsg = New System.Windows.Forms.Label()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(43, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(75, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter &age:"
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(124, 28)
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(36, 27)
+        Me.txtAge.TabIndex = 1
+        '
+        'lblMsg
+        '
+        Me.lblMsg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblMsg.Location = New System.Drawing.Point(47, 80)
+        Me.lblMsg.Name = "lblMsg"
+        Me.lblMsg.Size = New System.Drawing.Size(284, 57)
+        Me.lblMsg.TabIndex = 4
+        Me.lblMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(47, 163)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(145, 38)
+        Me.btnDisplay.TabIndex = 2
+        Me.btnDisplay.Text = "&Display Message"
+        Me.btnDisplay.UseVisualStyleBackColor = true
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(198, 163)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(133, 38)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = true
+        '
+        'frmMain
+        '
+        Me.AcceptButton = Me.btnDisplay
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8!, 20!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(379, 227)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.lblMsg)
+        Me.Controls.Add(Me.txtAge)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Voter Eligibility"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtAge As System.Windows.Forms.TextBox
+    Friend WithEvents lblMsg As System.Windows.Forms.Label
+    Friend WithEvents btnDisplay As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Voter Solution/Voter Project/Main Form.vb
@@ -0,0 +1,57 @@
+﻿' Name:         Voter Project
+' Purpose:      Displays a message based on the user's age and registration status.
+' Programmer:   Zachary Murphy on 2-23-2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtAge_Enter(sender As Object, e As EventArgs) Handles txtAge.Enter
+        txtAge.SelectAll()
+    End Sub
+
+    Private Sub txtAge_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtAge.KeyPress
+        ' allows only numbers and the Backspace key
+
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtAge_TextChanged(sender As Object, e As EventArgs) Handles txtAge.TextChanged
+        lblMsg.Text = String.Empty
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        ' displays a message
+
+        Const strTOO_YOUNG As String = "You are too young to vote."
+        Const strMUST_REGISTER As String =
+            "You must register before you can vote."
+        Const strCAN_VOTE As String = "You can vote."
+        Const strPROMPT As String = "Are you registered to vote?"
+        Dim intAge As Integer
+        Dim dlgButton As DialogResult
+
+        Integer.TryParse(txtAge.Text, intAge)
+
+        If intAge < 18 Then
+            lblMsg.Text = strTOO_YOUNG
+        Else
+            dlgButton = MessageBox.Show(strPROMPT, "Voter Eligibility", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+            If dlgButton = DialogResult.Yes Then
+                lblMsg.Text = strCAN_VOTE
+            Else
+                lblMsg.Text = strMUST_REGISTER
+            End If
+        End If
+
+
+    End Sub
+End Class
