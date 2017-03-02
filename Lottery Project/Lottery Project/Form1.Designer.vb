1e35bae2e3c2b9a61aac72ff82f127df6856e611
Zachary Murphy <ztmurphy21@gmail.com>
2017-03-01 19:39:23 -0500

    last bit of stuff

---
+++ Lottery Project/Lottery Project/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblNumbers = New System.Windows.Forms.Label()
+        Me.btnSelect = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Lottery_Project.My.Resources.Resources.BagofMoney1
+        Me.PictureBox1.Location = New System.Drawing.Point(70, 61)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(201, 173)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(327, 90)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(130, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Lottery Numbers:"
+        '
+        'lblNumbers
+        '
+        Me.lblNumbers.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblNumbers.Location = New System.Drawing.Point(327, 130)
+        Me.lblNumbers.Name = "lblNumbers"
+        Me.lblNumbers.Size = New System.Drawing.Size(364, 39)
+        Me.lblNumbers.TabIndex = 2
+        '
+        'btnSelect
+        '
+        Me.btnSelect.Location = New System.Drawing.Point(327, 193)
+        Me.btnSelect.Name = "btnSelect"
+        Me.btnSelect.Size = New System.Drawing.Size(192, 35)
+        Me.btnSelect.TabIndex = 3
+        Me.btnSelect.Text = "&Select Numbers"
+        Me.btnSelect.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(590, 193)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(97, 35)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(828, 324)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSelect)
+        Me.Controls.Add(Me.lblNumbers)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "MainForm"
+        Me.Text = "Lottery Numbers"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblNumbers As Label
+    Friend WithEvents btnSelect As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Lottery Project/Lottery Project/Form1.vb
@@ -0,0 +1,40 @@
+﻿' Lottery Project
+' Will output the six winning numbers for tonights lotter!
+'Programmer: Zachary Murphy on 2-27-2017
+
+Public Class MainForm
+    Private Sub btnSelect_Click(sender As Object, e As EventArgs) Handles btnSelect.Click
+        'variables needed
+        Dim randGen As New Random
+        Dim intNum1 As Integer
+        Dim intNum2 As Integer
+        Dim intNum3 As Integer
+        Dim intNum4 As Integer
+        Dim intNum5 As Integer
+        Dim intNum6 As Integer
+        Dim strSpace As String
+
+        'will use this to make output look neat.
+        strSpace = " , "
+
+        'lets draw the winning numbers randomly!
+        intNum1 = randGen.Next(1, 55)
+        intNum2 = randGen.Next(1, 55)
+        intNum3 = randGen.Next(1, 55)
+        intNum4 = randGen.Next(1, 55)
+        intNum5 = randGen.Next(1, 55)
+        intNum6 = randGen.Next(1, 55)
+
+        'output the winning numbers
+        lblNumbers.Text = intNum1.ToString + strSpace + intNum2.ToString + strSpace + intNum3.ToString + strSpace + intNum4.ToString + strSpace +
+            intNum5.ToString + strSpace + intNum6.ToString
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'close the application
+        Me.Close()
+
+    End Sub
+
+
+End Class
